dae3580c199fef40a3defcb0d3a82f98efa23a9f
unknown <tanmayghai20@gmail.com>
2023-07-04 23:37:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Task05 from "./components/Task05";
+function App() {
+  return (
+    <div className="App">
+      <Task05 />
+    </div>
+  );
+}
+
+export default App;
